746537157a0473cc03897924c49037c8776a1c06
Alaa Etoom <alaaatoom98@gmail.com>
2020-12-24 03:30:41 +0200

    initial backend server

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+const app = express() // the actuall express app
+dotenv.config() // to access the environment variables
+
+app.use(bodyParser.json({ limit: "30mb", extended: true })) // to parse all incoming requests to json
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+app.use(cors()) // to make access from anywhere
+
+// connecting to MongoDB
+const CONNECTION_URL = process.env.CONNECTION_URL
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+.then(() => console.log('Connected to MongoDB'))
+.catch((error) => console.log('Can\'t connect to MongoDB', error.message))
+
+// running the server
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
